3636dbf89109131b07f8a093a13327ce29fdc8d6
Alan Yao <alaney2@illinois.edu>
2024-03-25 17:04:14 -0500

    fib

--- binary exponentiation/fibonacci.py
+++ binary exponentiation/fibonacci.py
@@ -1,27 +1,24 @@
-t, m = map(int, input().split())
+import numpy as np
 
+t, m = map(int, input().split())
 for _ in range(t):
-  n = int(input())
-  
-  def multiply(a, b):
-    c = [[0, 0], [0, 0]]
-    for i in range(2):
-      for j in range(2):
-        for k in range(2):
-          c[i][j] += a[i][k] * b[k][j]
-          c[i][j] %= m
-    return c
-  
-  def power(a, n):
-    if n == 1:
-      return a
-    if n % 2 == 0:
-      return power(multiply(a, a), n // 2)
-    return multiply(a, power(multiply(a, a), n // 2))
-  
-  def fibonacci(n):
-    if n == 0 or n == 1:
-      return 1
-    return power([[1, 1], [1, 0]], n)[0][0]
-  
-  print(fibonacci(n) % m)
+    n = int(input())
+
+    def multiply(a, b):
+        c = np.dot(a, b)
+        c %= m
+        return c
+
+    def power(a, n):
+        if n == 1:
+            return a
+        if n % 2 == 0:
+            return power(multiply(a, a), n // 2)
+        return multiply(a, power(multiply(a, a), n // 2))
+
+    def fibonacci(n):
+        if n == 0 or n == 1:
+            return 1
+        return power(np.array([[1, 1], [1, 0]], dtype=object), n)[0, 0]
+
+    print(fibonacci(n) % m)
