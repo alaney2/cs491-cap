e749e1a467603200fa1562b737c0f99cece9a8ed
Alan Yao <alaney2@illinois.edu>
2024-03-29 15:28:52 -0500

    joty

--- gcd/joty.py
+++ gcd/joty.py
@@ -1,14 +1,11 @@
 n, a, b, p, q = map(int, input().split())
-
 a_multiple = n // a
 b_multiple = n // b
-common_multiple = n // (a * b)
 
-chocolates = 0
-
-if (p > q):
-  chocolates = (a_multiple * p) + (b_multiple * q) - (common_multiple * q)
-else:
-  chocolates = (a_multiple * p) + (b_multiple * q) - (common_multiple * p)
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a % b)
   
+lcm = (a * b) // gcd(a, b)
+chocolates = p * (a_multiple) + q * (b_multiple) - (min(p, q) * (n // lcm))
+
 print(chocolates)
